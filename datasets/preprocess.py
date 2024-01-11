39f5bed00b1b8fcda6604238d3a03cb085ab3a55
zzzmn917 <junnanzhuo@163.com>
2024-01-11 05:06:44 +0000

    注释-1.11

--- datasets/preprocess.py
+++ datasets/preprocess.py
@@ -25,20 +25,21 @@ if opt.dataset == 'diginetica':
 elif opt.dataset =='yoochoose':
     dataset = 'yoochoose-clicks.dat'
 
-print("-- Starting @ %ss" % datetime.datetime.now())  #hu
+print("-- Starting @ %ss" % datetime.datetime.now())  # 打印当前时间的消息，标志着代码执行的开始
 with open(dataset, "r") as f:
     if opt.dataset == 'yoochoose':
         reader = csv.DictReader(f, delimiter=',')
     else:
         reader = csv.DictReader(f, delimiter=';')
-    sess_clicks = {}
-    sess_date = {}
+    sess_clicks = {}  # 记录每个session点击过的物品
+    sess_date = {}   # 点击的时间
     ctr = 0
     curid = -1
     curdate = None
     for data in reader:
         sessid = data['session_id']
-        if curdate and not curid == sessid:
+        # 目的是处理日期数据，并将其转换成UNIX时间戳
+        if curdate and not curid == sessid: # 当前日期 curdate 不为空且当前session ID curid 不等于新的session ID sessid 时执行
             date = ''
             if opt.dataset == 'yoochoose':
                 date = time.mktime(time.strptime(curdate[:19], '%Y-%m-%dT%H:%M:%S'))
@@ -67,8 +68,8 @@ with open(dataset, "r") as f:
     else:
         date = time.mktime(time.strptime(curdate, '%Y-%m-%d'))
         for i in list(sess_clicks):
-            sorted_clicks = sorted(sess_clicks[i], key=operator.itemgetter(1))
-            sess_clicks[i] = [c[0] for c in sorted_clicks]
+            sorted_clicks = sorted(sess_clicks[i], key=operator.itemgetter(1))  # 对每个session的点击物品按照时间进行排序
+            sess_clicks[i] = [c[0] for c in sorted_clicks]  # 更新 sess_clicks 字典，只保留物品ID而不包含时间
     sess_date[curid] = date
 print("-- Reading data @ %ss" % datetime.datetime.now())
 
@@ -88,8 +89,9 @@ for s in sess_clicks:
         else:
             iid_counts[iid] = 1
 
-sorted_counts = sorted(iid_counts.items(), key=operator.itemgetter(1))
+sorted_counts = sorted(iid_counts.items(), key=operator.itemgetter(1)) # 将物品点击总数按序排列
 
+# 过滤掉点击物品数小于2的物品、所有session累计点击数小于5的物品
 length = len(sess_clicks)
 for s in list(sess_clicks):
     curseq = sess_clicks[s]
@@ -101,27 +103,28 @@ for s in list(sess_clicks):
         sess_clicks[s] = filseq
 
 # Split out test set based on dates
-dates = list(sess_date.items())
+dates = list(sess_date.items())   # 列表 dates，其中每个元素都是一个元组 (session_id, date)
 maxdate = dates[0][1]
 
+# maxdate 将包含 dates 列表中的最大日期值（多用于按时间线划分训练集和测试集）
 for _, date in dates:
     if maxdate < date:
         maxdate = date
 
-# 7 days for test
+# 1 or 7 days for test
 splitdate = 0
-if opt.dataset == 'yoochoose':
+if opt.dataset == 'yoochoose':  # yoochoose 划分1天
     splitdate = maxdate - 86400 * 1  # the number of seconds for a day：86400
-else:
+else:    # diginetica 划分7天
     splitdate = maxdate - 86400 * 7
 
 print('Splitting date', splitdate)      # Yoochoose: ('Split date', 1411930799.0)
-tra_sess = filter(lambda x: x[1] < splitdate, dates)
-tes_sess = filter(lambda x: x[1] > splitdate, dates)
+tra_sess = filter(lambda x: x[1] < splitdate, dates)  # 时间线之前是训练集
+tes_sess = filter(lambda x: x[1] > splitdate, dates)  # 时间线之后是测试集
 
 # Sort sessions by date
-tra_sess = sorted(tra_sess, key=operator.itemgetter(1))     # [(session_id, timestamp), (), ]
-tes_sess = sorted(tes_sess, key=operator.itemgetter(1))     # [(session_id, timestamp), (), ]
+tra_sess = sorted(tra_sess, key=operator.itemgetter(1))     # [(session_id, timestamp), (), ]，升序排序
+tes_sess = sorted(tes_sess, key=operator.itemgetter(1))     # [(session_id, timestamp), (), ]，升序排序
 print(len(tra_sess))    # 186670    # 7966257
 print(len(tes_sess))    # 15979     # 15324
 print(tra_sess[:3])
@@ -130,15 +133,15 @@ print("-- Splitting train set and test set @ %ss" % datetime.datetime.now())
 
 # Choosing item count >=5 gives approximately the same number of items as reported in paper
 item_dict = {}
-# Convert training sessions to sequences and renumber items to start from 1
+# Convert training sessions to sequences and renumber items to start from 1  重构训练集
 def obtian_tra():
     train_ids = []
-    train_seqs = []
+    train_seqs = []  # session对应的物品序列
     train_dates = []
     item_ctr = 1
-    for s, date in tra_sess:
+    for s, date in tra_sess:  # s 是session的id
         seq = sess_clicks[s]
-        outseq = []
+        outseq = []  # 初始化一个空列表，用于存储重新编号后的物品序列
         for i in seq:
             if i in item_dict:
                 outseq += [item_dict[i]]
@@ -151,10 +154,10 @@ def obtian_tra():
         train_ids += [s]
         train_dates += [date]
         train_seqs += [outseq]
-    print(item_ctr)     # 43098, 37484
+    print(item_ctr)     # 43098, 37484  重新编号后的物品总数
     return train_ids, train_dates, train_seqs
 
-
+# 测试集中只保留那些在训练集中出现过的物品
 # Convert test sessions to sequences, ignoring items that do not appear in training set
 def obtian_tes():
     test_ids = []
@@ -164,7 +167,7 @@ def obtian_tes():
         seq = sess_clicks[s]
         outseq = []
         for i in seq:
-            if i in item_dict:
+            if i in item_dict:  # item_dict 是用于存储训练集物品（items）的字典
                 outseq += [item_dict[i]]
         if len(outseq) < 2:
             continue
@@ -177,7 +180,7 @@ def obtian_tes():
 tra_ids, tra_dates, tra_seqs = obtian_tra()
 tes_ids, tes_dates, tes_seqs = obtian_tes()
 
-
+# 处理序列
 def process_seqs(iseqs, idates):
     out_seqs = []
     out_dates = []
@@ -186,17 +189,17 @@ def process_seqs(iseqs, idates):
     for id, seq, date in zip(range(len(iseqs)), iseqs, idates):
         for i in range(1, len(seq)):
             tar = seq[-i]
-            labs += [tar]
+            labs += [tar]  # labs 中存放的是原序列中最后一个物品
             out_seqs += [seq[:-i]]
             out_dates += [date]
             ids += [id]
-    return out_seqs, out_dates, labs, ids
+    return out_seqs, out_dates, labs, ids  # 最后输出 新序列（去掉labs）、日期、标签（原序列的最后一个物品）、商品id
 
-
+# tra_seqs 是原训练集序列、 tr_seqs是新训练集序列
 tr_seqs, tr_dates, tr_labs, tr_ids = process_seqs(tra_seqs, tra_dates)
 te_seqs, te_dates, te_labs, te_ids = process_seqs(tes_seqs, tes_dates)
-tra = (tr_seqs, tr_labs)
-tes = (te_seqs, te_labs)
+tra = (tr_seqs, tr_labs)  # 最终需要的训练集（去掉最后一个物品的新序列 + 最后一个目标物品）
+tes = (te_seqs, te_labs)  # 同上
 print(len(tr_seqs))
 print(len(te_seqs))
 print(tr_seqs[:3], tr_dates[:3], tr_labs[:3])
@@ -207,21 +210,22 @@ for seq in tra_seqs:
     all += len(seq)
 for seq in tes_seqs:
     all += len(seq)
-print('avg length: ', all/(len(tra_seqs) + len(tes_seqs) * 1.0))
-if opt.dataset == 'diginetica':
+print('avg length: ', all/(len(tra_seqs) + len(tes_seqs) * 1.0))  # 求平均序列长度
+if opt.dataset == 'diginetica':  # 该数据集，保存训练集和测试集的处理结果以及所有训练集的原始序列
     if not os.path.exists('diginetica'):
         os.makedirs('diginetica')
     pickle.dump(tra, open('diginetica/train.txt', 'wb'))
     pickle.dump(tes, open('diginetica/test.txt', 'wb'))
     pickle.dump(tra_seqs, open('diginetica/all_train_seq.txt', 'wb'))
-elif opt.dataset == 'yoochoose':
+elif opt.dataset == 'yoochoose':  # 针对该数据集，创建两个名字的目录，测试集对于两者来说是一样的
     if not os.path.exists('yoochoose1_4'):
         os.makedirs('yoochoose1_4')
     if not os.path.exists('yoochoose1_64'):
         os.makedirs('yoochoose1_64')
     pickle.dump(tes, open('yoochoose1_4/test.txt', 'wb'))
     pickle.dump(tes, open('yoochoose1_64/test.txt', 'wb'))
-
+    
+    # 对于 yoochoose 训练集，将其分为两个子集，一个是原始训练集的四分之一，另一个是原始训练集的六十四分之一
     split4, split64 = int(len(tr_seqs) / 4), int(len(tr_seqs) / 64)
     print(len(tr_seqs[-split4:]))
     print(len(tr_seqs[-split64:]))
@@ -235,7 +239,7 @@ elif opt.dataset == 'yoochoose':
     pickle.dump(tra64, open('yoochoose1_64/train.txt', 'wb'))
     pickle.dump(seq64, open('yoochoose1_64/all_train_seq.txt', 'wb'))
 
-else:
+else:   # 如果数据集是其他类型
     if not os.path.exists('sample'):
         os.makedirs('sample')
     pickle.dump(tra, open('sample/train.txt', 'wb'))
--- pytorch_code/utils.py
+++ pytorch_code/utils.py
@@ -9,9 +9,10 @@ Created on July, 2018
 import networkx as nx
 import numpy as np
 
-
+# 为训练集中的序列构建有向图，并且为其中的边赋予权重（见论文记录本内容）
 def build_graph(train_data):
     graph = nx.DiGraph()
+    # 构建有向图
     for seq in train_data:
         for i in range(len(seq) - 1):
             if graph.get_edge_data(seq[i], seq[i + 1]) is None:
@@ -19,6 +20,7 @@ def build_graph(train_data):
             else:
                 weight = graph.get_edge_data(seq[i], seq[i + 1])['weight'] + 1
             graph.add_edge(seq[i], seq[i + 1], weight=weight)
+    # 用于规范化入边的权重（指向同一节点的所有边权重之和为1）        
     for node in graph.nodes:
         sum = 0
         for j, i in graph.in_edges(node):
@@ -28,30 +30,31 @@ def build_graph(train_data):
                 graph.add_edge(j, i, weight=graph.get_edge_data(j, i)['weight'] / sum)
     return graph
 
-
-def data_masks(all_usr_pois, item_tail):
+# 补全session长度
+def data_masks(all_usr_pois, item_tail):  # all_usr_pois: 一个包含用户行为序列的列表；item_tail: 表示用于填充不足长度的虚拟项目
     us_lens = [len(upois) for upois in all_usr_pois]
-    len_max = max(us_lens)
-    us_pois = [upois + item_tail * (len_max - le) for upois, le in zip(all_usr_pois, us_lens)]
-    us_msks = [[1] * le + [0] * (len_max - le) for le in us_lens]
+    len_max = max(us_lens) # 用户行为序列的最大长度
+    us_pois = [upois + item_tail * (len_max - le) for upois, le in zip(all_usr_pois, us_lens)] # 处理后的用户行为序列列表，每个序列的长度都被填充到最大长度
+    us_msks = [[1] * le + [0] * (len_max - le) for le in us_lens] # 表示用户行为序列的实际长度
     return us_pois, us_msks, len_max
 
-
-def split_validation(train_set, valid_portion):
+# 用于将训练集划分为训练集和测试集
+def split_validation(train_set, valid_portion): # valid_portion：表示测试集占总训练集的比例。
     train_set_x, train_set_y = train_set
     n_samples = len(train_set_x)
     sidx = np.arange(n_samples, dtype='int32')
-    np.random.shuffle(sidx)
+    np.random.shuffle(sidx)   #上面两行代码，用于随机打乱训练集的顺序
     n_train = int(np.round(n_samples * (1. - valid_portion)))
     valid_set_x = [train_set_x[s] for s in sidx[n_train:]]
     valid_set_y = [train_set_y[s] for s in sidx[n_train:]]
-    train_set_x = [train_set_x[s] for s in sidx[:n_train]]
-    train_set_y = [train_set_y[s] for s in sidx[:n_train]]
+    train_set_x = [train_set_x[s] for s in sidx[:n_train]]  # 划分后的测试集输入
+    train_set_y = [train_set_y[s] for s in sidx[:n_train]]  # 测试集的输出
 
     return (train_set_x, train_set_y), (valid_set_x, valid_set_y)
 
 
 class Data():
+    # 初始化，对输入的数据进行预处理
     def __init__(self, data, shuffle=False, graph=None):
         inputs = data[0]
         inputs, mask, len_max = data_masks(inputs, [0])
@@ -62,7 +65,8 @@ class Data():
         self.length = len(inputs)
         self.shuffle = shuffle
         self.graph = graph
-
+    
+    # 用于生成批次数据，可以指定批次大小
     def generate_batch(self, batch_size):
         if self.shuffle:
             shuffled_arg = np.arange(self.length)
@@ -76,30 +80,31 @@ class Data():
         slices = np.split(np.arange(n_batch * batch_size), n_batch)
         slices[-1] = slices[-1][:(self.length - batch_size * (n_batch - 1))]
         return slices
-
+    
+    # 对输入的处理包括获取唯一项目、构建邻接矩阵等
     def get_slice(self, i):
         inputs, mask, targets = self.inputs[i], self.mask[i], self.targets[i]
         items, n_node, A, alias_inputs = [], [], [], []
-        for u_input in inputs:
-            n_node.append(len(np.unique(u_input)))
+        for u_input in inputs:  # np.unique(u_input) 来获取每个用户输入序列中的唯一项目
+            n_node.append(len(np.unique(u_input))) 
         max_n_node = np.max(n_node)
         for u_input in inputs:
             node = np.unique(u_input)
-            items.append(node.tolist() + (max_n_node - len(node)) * [0])
-            u_A = np.zeros((max_n_node, max_n_node))
+            items.append(node.tolist() + (max_n_node - len(node)) * [0]) # 将当前用户输入序列中的项目列表转换为一个固定长度的列表（max_n_node），如果不足的话，用 0 补齐
+            u_A = np.zeros((max_n_node, max_n_node)) # 创建一个零矩阵，用于存储邻接矩阵
             for i in np.arange(len(u_input) - 1):
                 if u_input[i + 1] == 0:
                     break
                 u = np.where(node == u_input[i])[0][0]
                 v = np.where(node == u_input[i + 1])[0][0]
-                u_A[u][v] = 1
+                u_A[u][v] = 1 # 在邻接矩阵中标记节点 u 指向节点 v 的关系，即建立了两个项目之间的邻接关系
             u_sum_in = np.sum(u_A, 0)
-            u_sum_in[np.where(u_sum_in == 0)] = 1
-            u_A_in = np.divide(u_A, u_sum_in)
+            u_sum_in[np.where(u_sum_in == 0)] = 1  # 计算节点入度的权重总和，若是0则写为1
+            u_A_in = np.divide(u_A, u_sum_in) # 计算输入节点的归一化权重矩阵，即将每个节点的入度按总和进行归一化
             u_sum_out = np.sum(u_A, 1)
-            u_sum_out[np.where(u_sum_out == 0)] = 1
-            u_A_out = np.divide(u_A.transpose(), u_sum_out)
+            u_sum_out[np.where(u_sum_out == 0)] = 1  # 同上
+            u_A_out = np.divide(u_A.transpose(), u_sum_out)  # 归一化输出节点的权重矩阵
             u_A = np.concatenate([u_A_in, u_A_out]).transpose()
-            A.append(u_A)
-            alias_inputs.append([np.where(node == i)[0][0] for i in u_input])
-        return alias_inputs, A, items, mask, targets
+            A.append(u_A)  # A：归一化后的出入度邻接矩阵
+            alias_inputs.append([np.where(node == i)[0][0] for i in u_input]) # alias_input :session中物品对应于去重排序后的session的id
+        return alias_inputs, A, items, mask, targets # items：排序去重补全后的序列； mask：填充后的session,item id换为1，填充位是0
